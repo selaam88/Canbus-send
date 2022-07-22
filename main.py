cc1667b9c597f889b1a183aeb32c87db53b2bd3d
selaam <s.almujayyaz@gmail.com>
2022-07-22 13:11:59 +0200

    initial commit 2

---
+++ main.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4
+# ls -l /sys/bus/usb-serial/devices
+# sudo slcand -o -s6 -t hw -S 3000000 /dev/ttyUSB0D
+
+
+"""
+Example for sending CAN frames via ASCII protocol (slcan).
+"""
+
+import sys
+import time
+
+import can
+import serial
+
+CAN_DEV = '/dev/ttyUSB1@3000000'
+
+
+def main():
+    """main routine."""
+    try:
+        bus = can.interface.Bus(bustype='slcan',
+                                channel=CAN_DEV,
+                                rtscts=True,
+                                bitrate=1000000)
+    except serial.serialutil.SerialException as err:
+        print(err)
+        sys.exit(1)
+
+    # send standard frame
+    msg = can.Message(arbitration_id=0x100,
+                      is_extended_id=False,
+                      data=[0x00, 0x01, 0x02, 0x03])
+    bus.send(msg)
+
+    # send extended frame
+    msg = can.Message(arbitration_id=0x100,
+                      is_extended_id=True,
+                      data=[0x00, 0x01, 0x02, 0x03])
+    bus.send(msg)
+
+    # send RTR frame
+    msg = can.Message(arbitration_id=0x100,
+                      is_extended_id=True,
+                      is_remote_frame=True)
+    bus.send(msg)
+
+    time.sleep(5)
+
+    bus.shutdown()
+
+
+if __name__ == '__main__':
+    main()
